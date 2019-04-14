d83ec574fe1311cbe8cafabd00d33cea566883a7
Semiion Morozov <49210791+SemiionMorozov@users.noreply.github.com>
2019-04-14 23:47:07 +0500

    Add files via upload

---
+++ 7. Stack_14.04.2019.py
@@ -0,0 +1,28 @@
+# класс Работа со стеком - 14.04.2019 - semiion-morozov@mail.ru
+
+# класс стека
+class Stack :
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):   # загрузить в стек
+        self.items.append(item)
+
+    def pop(self):          # выгрузить из стека
+        return self.items.pop()
+
+
+stack = Stack()     # инициализация стека
+
+for i in range(5):  # заполнение стека
+    stack.push(i)   # через цикл for
+
+print(stack.items)  # посмотреть содержимое стека
+
+a = 0               # выгрузка из стека
+while a < len(stack.items): # через цикл while
+    print(stack.pop())
+    ++a
+
+print(stack.items)  # посмотреть содержимое стека
+
